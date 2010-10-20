b68d332ddf296c657b924b022cdfd9ebf8d5f789
Leonardo Varuzza <varuzza@gmail.com>
2010-10-20 12:33:38 -0200

    Add Hbase code

--- src/main/java/com/lifetech/hadoop/bioseq/BioSeqWritable.java
+++ src/main/java/com/lifetech/hadoop/bioseq/BioSeqWritable.java
@@ -4,7 +4,6 @@ import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
-import org.apache.hadoop.io.ByteWritable;
 import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
