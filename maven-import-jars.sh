1d1acffb404847bd77d45aed141a8964f6e83a78
Leonardo Varuzza <varuzza@gmail.com>
2010-10-24 09:29:33 -0200

    Add non maven libs

--- maven-import-jars.sh
+++ maven-import-jars.sh
@@ -1,2 +1,2 @@
-mvn install:install-file -Dfile=/usr/local/hadoop/lib/hadoop-gpl-compression-0.2.0-dev.jar -DgroupId=org.apache.hadoop -DartifactId=hadoop-gpl-compresion -Dversion=0.2.0 -DgeneratedPom=true -Dpackaging=jar
-mvn install:install-file -Dfile=/usr/local/hbase/hbase-0.20.6.jar -DgroupId=org.apache.hbase -DartifactId=hbase -Dversion=0.20.6 -DgeneratedPom=true -Dpackaging=jar
+mvn install:install-file -Dfile=./lib/hadoop-gpl-compression-0.2.0-dev.jar -DgroupId=org.apache.hadoop -DartifactId=hadoop-gpl-compresion -Dversion=0.2.0 -DgeneratedPom=true -Dpackaging=jar
+mvn install:install-file -Dfile=./lib/hbase-0.20.6.jar -DgroupId=org.apache.hbase -DartifactId=hbase -Dversion=0.20.6 -DgeneratedPom=true -Dpackaging=jar
