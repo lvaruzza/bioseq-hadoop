18f3eb4ad9863917d1fa81cc10691b119d9e77e5
Leonardo Varuzza <varuzza@gmail.com>
2010-12-02 14:35:35 -0200

    DumpSpectrum can be used with files and directories.

--- src/main/java/com/lifetech/hadoop/bioseq/spectrum/DumpKmers.java
+++ src/main/java/com/lifetech/hadoop/bioseq/spectrum/DumpKmers.java
@@ -11,19 +11,18 @@ import org.apache.hadoop.io.IOUtils;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.SequenceFile;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.fs.FileStatus;
 
 import com.lifetech.hadoop.bioseq.FourBitsEncoder;
 
 
 public class DumpKmers {
-	public static void main(String[] args) throws IOException {
-		String uri = args[0];
-		Configuration conf = new Configuration();
-		FileSystem fs = FileSystem.get(URI.create(uri), conf);
-		Path path = new Path(uri);
+	private FourBitsEncoder enc = new FourBitsEncoder(); 
+	private FileSystem fs;
+	private Configuration conf;
+	
+	private void dumpFile(Path path) throws IOException {
 		SequenceFile.Reader reader = null;
-		FourBitsEncoder enc = new FourBitsEncoder(); 
-		
 		try {
 			reader = new SequenceFile.Reader(fs, path, conf);
 			BytesWritable key = new BytesWritable();
@@ -31,11 +30,29 @@ public class DumpKmers {
 			
 			while (reader.next(key, value)) {			
 				Text kmer = enc.decode(key); 
-				enc.printBytes(key.getBytes(), key.getLength());
-				System.out.printf("\t%s\t%d\n", kmer.toString(),value.get());
+				//enc.printBytes(key.getBytes(), key.getLength());
+				System.out.printf("%s\t%d\n", kmer.toString(),value.get());
 			}
 		} finally {
 			IOUtils.closeStream(reader);
+		}		
+	}
+	
+	private void run(String uri) throws IOException {
+		conf = new Configuration();
+		fs = FileSystem.get(URI.create(uri), conf);
+		Path path = new Path(uri);
+		if (fs.getFileStatus(path).isDir()) {
+			FileStatus[] files = fs.listStatus(path);
+			for (FileStatus file : files) {
+				dumpFile(file.getPath());
+			}
+		} else {
+			dumpFile(path);
 		}
+	}
+	public static void main(String[] args) throws IOException {
+		String uri = args[0];
+		new DumpKmers().run(uri);
 	}
 }
