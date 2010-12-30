7368dc1dcbf013e9d454d3cdeb1f5c69b8a0baee
Leonardo Varuzza <varuzza@gmail.com>
2010-12-30 21:21:44 -0200

    New strategy to do the FastaQualOutput, write qual and fasta using the same committer.

--- src/main/java/com/lifetech/hadoop/mapreduce/output/FastaQualOutputFormat.java
+++ src/main/java/com/lifetech/hadoop/mapreduce/output/FastaQualOutputFormat.java
@@ -20,7 +20,8 @@ import org.apache.hadoop.util.ReflectionUtils;
 import com.lifetech.hadoop.bioseq.BioSeqWritable;
 import com.lifetech.utils.PathUtils;
 
-public class FastaQualOutputFormat extends FileOutputFormat<NullWritable, BioSeqWritable> {
+public class FastaQualOutputFormat extends
+		FileOutputFormat<NullWritable, BioSeqWritable> {
 	@Override
 	public RecordWriter<NullWritable, BioSeqWritable> getRecordWriter(
 			TaskAttemptContext job) throws IOException, InterruptedException {
@@ -44,40 +45,39 @@ public class FastaQualOutputFormat extends FileOutputFormat<NullWritable, BioSeq
 		if (!isCompressed) {
 			FSDataOutputStream fastaOut = fs.create(fastaFile, false);
 			FSDataOutputStream qualOut = fs.create(qualFile, false);
-			return new FastaQualRecordWriter(fastaOut,qualOut);
+			return new FastaQualRecordWriter(fastaOut, qualOut);
 		} else {
 			FSDataOutputStream fastaOut = fs.create(fastaFile, false);
 			FSDataOutputStream qualOut = fs.create(qualFile, false);
-			return new FastaQualRecordWriter(new DataOutputStream(codec.createOutputStream(fastaOut)),
-											 new DataOutputStream(codec.createOutputStream(fastaOut)));
+			return new FastaQualRecordWriter(new DataOutputStream(
+					codec.createOutputStream(fastaOut)), new DataOutputStream(
+					codec.createOutputStream(fastaOut)));
 		}
 	}
-	
-	  private FileOutputCommitter qualCommitter = null;
-	
-	  /**
-	   * Get the default path and filename for the output format.
-	   * @param context the task context
-	   * @param extension an extension to add to the filename
-	   * @return a full path $output/_temporary/$taskid/part-[mr]-$id
-	   * @throws IOException
-	   */
-	  public Path getDefaultWorkQualFile(TaskAttemptContext context,
-	                                 String extension) throws IOException{
-	    FileOutputCommitter committer = 
-	      (FileOutputCommitter) getOutputQualCommitter(context);
-	    return new Path(committer.getWorkPath(), getUniqueFile(context, "part", 
-	                                                           extension));
-	  }
 
-	  public synchronized 
-	     OutputCommitter getOutputQualCommitter(TaskAttemptContext context
-	                                        ) throws IOException {
-	    if (qualCommitter == null) {
-	      Path output = PathUtils.changePathExtension(getOutputPath(context),".qual");
-	      qualCommitter = new FileOutputCommitter(output, context);
-	    }
-	    return qualCommitter;
-	  }
-	
+	/**
+	 * Get the default path and filename for the output format.
+	 * 
+	 * @param context
+	 *            the task context
+	 * @param extension
+	 *            an extension to add to the filename
+	 * @return a full path $output/_temporary/$taskid/part-[mr]-$id
+	 * @throws IOException
+	 */
+	public Path getDefaultWorkQualFile(TaskAttemptContext context,
+			String extension) throws IOException {
+		FileOutputCommitter committer = (FileOutputCommitter) getOutputCommitter(context);
+		return new Path(committer.getWorkPath(), getUniqueFile(context, "qual",
+				extension));
+	}
+
+	@Override
+	public Path getDefaultWorkFile(TaskAttemptContext context,
+			String extension) throws IOException {
+		FileOutputCommitter committer = (FileOutputCommitter) getOutputCommitter(context);
+		return new Path(committer.getWorkPath(), getUniqueFile(context, "fasta",
+				extension));
+	}
+
 }
